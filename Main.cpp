e2f817ea49b4a60f48503c7c4edf5bb96b34d021
Renangsa <rgsa@cin.ufpe.br>
2024-11-20 17:13:20 -0300

    Cmake created

--- Main.cpp
+++ Main.cpp
@@ -1,34 +1,29 @@
-#include <iostream>
-#include <random>
-#include <stdlib.h>
 #include <chrono>
+#include <print>
+#include <cstdlib>
+#include <iostream>
 
-using namespace std;
+
+using std::cout;
+using std::cin;
+using std::chrono::steady_clock;
 
 class GameMode
 {
-    public:
-    int chances;
-    int tries;
-    string difficulty;
-    bool exit;
-    short seconds;
-    GameMode()
-    {
+ public:
+    void resetGameCache() {
         tries = 0;
         chances = 0;
         seconds = 0;
         difficulty = "None";
-        exit = false;
     }
 
-    void resetGameCache()
-    {
-        tries = 0;
-        chances = 0;
-        seconds = 0;
-        difficulty = "None";
-    }
+    int tries = 0;
+    bool exit = false;
+    int chances = 0;
+    double seconds = 0.0;
+    std::string difficulty = "None";
+
 };
 /*class Player
 {
@@ -52,27 +47,26 @@ int checkInput(int input)
 void runGame(GameMode &gameInfo)
 {
     if(!gameInfo.exit){
-        int randomNum= 1+ (rand() % 100);
-        int input;
+        int random_num = 1+ (rand() % 100);
+        int input{};
         gameInfo.resetGameCache();
 
-
-        cout << "Bem-vindo ao jogo de adivinhação de números!\n";
-        cout << "Digite 0 para: Acessar regras!\n";
-        cout << "Digite 1 para: Jogar!\n";
-        cout << "Digite 2 para: Sair!\n";
-        cout << "Aguardando resposta: ";
+        cout << "Bem-vindo ao jogo de adivinhação de números!\n"
+                "Digite 0 para: Acessar regras!\n"
+                "Digite 1 para: Jogar!\n"
+                "Digite 2 para: Sair!\n"
+                "Aguardando resposta: ";
         cin >> input;
 
         switch (checkInput(input))
         {
             case 0:
-                cout << "-> O computador escolherá aleatoriamente um número secreto entre 1 e 100.\n";
-                cout << "-> Seu objetivo é adivinhar qual é esse número.\n";
-                cout << "-> Você terá um número limitado de tentativas para acertar.\n";
-                cout << "-> Se você adivinhar corretamente, o jogo termina e você vence!\n";
-                cout << "-> Caso contrário, o jogo continuará até que suas tentativas se esgotem e você perca.\n";
-                cout << "-> Boa sorte!\n";
+                cout << "-> O computador escolherá aleatoriamente um número secreto entre 1 e 100.\n"
+                        "-> Seu objetivo é adivinhar qual é esse número.\n"
+                        "-> Você terá um número limitado de tentativas para acertar.\n"
+                        "-> Se você adivinhar corretamente, o jogo termina e você vence!\n"
+                        "-> Caso contrário, o jogo continuará até que suas tentativas se esgotem e você perca.\n"
+                        "-> Boa sorte!\n";
                 system("Pause");
                 system("CLS");
                 cout << "-------------------------------------------------------------------------------------\n\n";
@@ -106,8 +100,10 @@ void runGame(GameMode &gameInfo)
                         system("Pause");
                         break;
                 }
-                chrono::steady_clock::time_point clock_begin = chrono::steady_clock::now();
-                cout << "A dificuldade selecionada foi o modo " << gameInfo.difficulty << endl;
+                steady_clock::time_point clock_begin = steady_clock::now();
+                // cout << "A dificuldade selecionada foi o modo " << gameInfo.difficulty << '\n';
+
+                std::print("A dificuldade selecionada foi o modo {}\n", gameInfo.difficulty);
                 while (gameInfo.chances > 0) //Jogando
                 {
                     gameInfo.tries++;
@@ -115,29 +111,26 @@ void runGame(GameMode &gameInfo)
                     cout << "Digite seu palpite: ";
                     cin >> input;
                     input = checkInput(input);
-                    if (input == randomNum)
+                    if (input == random_num)
                     {
-                        chrono::steady_clock::time_point clock_end = chrono::steady_clock::now(); // O tempo desde que escolheu a dificuldade até acertar
-                        chrono::steady_clock::duration time_span = clock_end - clock_begin;
-                        auto seconds = double(time_span.count()) * chrono::steady_clock::period::num / chrono::steady_clock::period::den;
+                        steady_clock::time_point clock_end = steady_clock::now(); // O tempo desde que escolheu a dificuldade até acertar
+                        steady_clock::duration time_span = clock_end - clock_begin;
+                        auto seconds = double(time_span.count()) * steady_clock::period::num / steady_clock::period::den;
                         cout << "Parabéns! Você acertou o número correto em " << gameInfo.tries << " tentativa(s)\n";
                         cout << "Seu tempo foi de: " << seconds << " segundos\n";
                         break;
                     }
-                    else
+                    if (input > random_num)
                     {
-                        if (input > randomNum)
-                        {
-                            cout << "Incorreto! O número que estou pensando é menor que " << input << endl;
-                        }
-                        else if (input < randomNum)
-                        {
-                            cout << "Incorreto! O número que estou pensando é maior que " << input << endl;
-                        }
+                        cout << "Incorreto! O número que estou pensando é menor que " << input << '\n';
+                    }
+                    else if (input < random_num)
+                    {
+                        cout << "Incorreto! O número que estou pensando é maior que " << input << '\n';
                     }
                     gameInfo.chances--;
                 }
-                cout << "O número sorteado foi " << randomNum << "!\n";
+                cout << "O número sorteado foi " << random_num << "!\n";
                 cout << "Deseja jogar novamente? ";
                 cout << "1 - (Sim) | 2 - (Não)\n";
                 cout << "Aguardando resposta: ";
